771ecec18995bf166992b620fecee1578a0813eb
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-04-07 03:50:37 -0300

    Implementação: Implementação do método UpdateAlunos, que recebe e insere a lista de alunos que pegamos do banco de dados

--- app/src/main/java/dev/jvitor/gerenciadordematriculas/screens/adapter/MainAdapter.kt
+++ app/src/main/java/dev/jvitor/gerenciadordematriculas/screens/adapter/MainAdapter.kt
@@ -1,19 +1,34 @@
 package dev.jvitor.gerenciadordematriculas.screens.adapter
 
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import dev.jvitor.gerenciadordematriculas.databinding.CardModelBinding
+import dev.jvitor.gerenciadordematriculas.models.Aluno
 import dev.jvitor.gerenciadordematriculas.screens.viewholder.MainViewHolder
 
 class MainAdapter : RecyclerView.Adapter<MainViewHolder>() {
+
+    private var listAluno: List<Aluno> = listOf()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
-        TODO("Not yet implemented")
+        val item = CardModelBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MainViewHolder(item)
     }
 
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
-        TODO("Not yet implemented")
+       holder.bind(listAluno[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return listAluno.count()
     }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateAlunos(list: List<Aluno>){
+        listAluno = list
+        notifyDataSetChanged()
+    }
+
 }
