7bce3f51b2a153aabe9e9da2c364ca7f0acc4f23
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-04-07 03:51:15 -0300

    Implementação: Implementação da viewModel para pegar a lista de alunos do banco de dados

--- app/src/main/java/dev/jvitor/gerenciadordematriculas/screens/MainActivity.kt
+++ app/src/main/java/dev/jvitor/gerenciadordematriculas/screens/MainActivity.kt
@@ -6,13 +6,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import dev.jvitor.gerenciadordematriculas.R
 import dev.jvitor.gerenciadordematriculas.databinding.ActivityMainBinding
 import dev.jvitor.gerenciadordematriculas.screens.adapter.MainAdapter
+import dev.jvitor.gerenciadordematriculas.screens.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val adapter = MainAdapter()
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,17 +28,24 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        // Layout
+        binding.recyclerList.layoutManager = LinearLayoutManager(this)
+        // adapter
+        binding.recyclerList.adapter = adapter
 
-        binding.btnScreenAdd.setOnClickListener{
-            startActivity(Intent(this, AddActivity::class.java ))
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        adapter.updateAlunos(viewModel.getAll())
+
+
+        binding.btnScreenAdd.setOnClickListener {
+            startActivity(Intent(this, AddActivity::class.java))
         }
 
-        // Layout
-     //   binding.recyclerList.layoutManager = LinearLayoutManager(this)
+    }
 
-        // adapter
-      //  binding.recyclerList.adapter = adapter
-
-
+    override fun onResume() {
+        super.onResume()
+        adapter.updateAlunos(viewModel.getAll())
     }
 }
