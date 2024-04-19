95a793330b7664fde73584b9486a9ea284e1731d
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-04-19 01:05:01 -0300

    Implementação: Adicionado uma variavel para observar a lista e atribuir instantaneamente a lista atualizada na MainViewModel.kt

---
+++ app/src/main/java/dev/jvitor/gerenciadordematriculas/view/viewmodel/MainViewModel.kt
@@ -0,0 +1,32 @@
+package dev.jvitor.gerenciadordematriculas.view.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import dev.jvitor.gerenciadordematriculas.model.Aluno
+import dev.jvitor.gerenciadordematriculas.model.repository.AlunoRepository
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository = AlunoRepository(application.applicationContext)
+
+    private val listAlunos = MutableLiveData<List<Aluno>>()
+    val alunos: LiveData<List<Aluno>> = listAlunos
+
+    fun getAll(){
+        listAlunos.value = repository.getAll()
+    }
+
+    fun quantity(): Int{
+        return repository.getAll().size
+    }
+
+    fun delete(cpf: String) {
+        repository.delete(cpf)
+    }
+
+    fun get(cpf: String) : Aluno{
+        return repository.get(cpf)
+    }
+}
