e016d7c7f0b4c345080ff78e36f5f3bb383d1ea8
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-04-19 00:46:05 -0300

    Implementação: Adicionado e implementado o método que vai ser chamado ao clicar em atualizar e adicionando os dados do aluno aos hints dos campos da tela de atualizar para que fique mais claro ao usuario

--- app/src/main/java/dev/jvitor/gerenciadordematriculas/view/UpdateActivity.kt
+++ app/src/main/java/dev/jvitor/gerenciadordematriculas/view/UpdateActivity.kt
@@ -1,4 +1,4 @@
-package dev.jvitor.gerenciadordematriculas.screens
+package dev.jvitor.gerenciadordematriculas.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -8,12 +8,13 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import dev.jvitor.gerenciadordematriculas.R
 import dev.jvitor.gerenciadordematriculas.databinding.ActivityUpdateBinding
-import dev.jvitor.gerenciadordematriculas.models.Aluno
-import dev.jvitor.gerenciadordematriculas.screens.viewmodel.UpdateViewModel
+import dev.jvitor.gerenciadordematriculas.model.Aluno
+import dev.jvitor.gerenciadordematriculas.view.viewmodel.UpdateViewModel
 
 class UpdateActivity : AppCompatActivity() {
     private lateinit var binding: ActivityUpdateBinding
     private lateinit var viewModel: UpdateViewModel
+    private lateinit var cpf: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUpdateBinding.inflate(layoutInflater)
@@ -24,22 +25,38 @@ class UpdateActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         viewModel = ViewModelProvider(this)[UpdateViewModel::class.java]
 
         binding.buttonUpdate.setOnClickListener {
             doUpdate()
         }
-        var cpf = binding.editCpf.text.toString()
-        cpf = "Valor não editavel"
 
+        valuesFields()
 
     }
+
     fun doUpdate(){
-        val cpf = binding.editCpf.toString()
         val name = binding.editName.text.toString()
         val sport = binding.editSport.text.toString()
         val day = binding.editDay.text.toString()
-        viewModel.update(Aluno(cpf, name, sport, day))
+        if (viewModel.validation(applicationContext.applicationContext, Aluno(cpf, name, sport, day)))
+            finish()
+        else
+            return
+    }
+
+    // Atribuindo os valores da intent recebida da MainActivity para ser mostrada nos hints
+    private fun valuesFields(){
+        cpf = intent?.extras?.getString("cpf") ?: throw IllegalStateException("cpf not found")
+        val name = intent?.extras?.getString("name") ?: throw IllegalStateException("name not found")
+        val sport = intent?.extras?.getString("sport") ?: throw IllegalStateException("sport not found")
+        val day = intent?.extras?.getString("day") ?: throw IllegalStateException("day not found")
+
+        binding.editCpf.hint = cpf
+        binding.editName.hint = name
+        binding.editSport.hint = sport
+        binding.editDay.hint = day
     }
 
 }
