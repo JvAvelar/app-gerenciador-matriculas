351da52c32044e7f14ce2b9607ec33832e391ab9
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-04-20 15:18:31 -0300

    Refatoração: Alterado os atributos da classe Aluno.kt escritos hardcoded por constantes fixas da classe Constants.kt

--- app/src/main/java/dev/jvitor/gerenciadordematriculas/model/Aluno.kt
+++ app/src/main/java/dev/jvitor/gerenciadordematriculas/model/Aluno.kt
@@ -7,8 +7,8 @@ import androidx.room.PrimaryKey
 @Entity
 data class Aluno(
     @PrimaryKey
-    @ColumnInfo(name = "cpf") val cpf: String,
-    @ColumnInfo(name = "nome") val name: String,
-    @ColumnInfo(name = "esporte") val sport: String,
-    @ColumnInfo(name = "dia") val day: String
+    @ColumnInfo(name = Constants.Attributs.CPF) val cpf: String,
+    @ColumnInfo(name =  Constants.Attributs.NAME) val name: String,
+    @ColumnInfo(name =  Constants.Attributs.SPORT) val sport: String,
+    @ColumnInfo(name =  Constants.Attributs.DAY) val day: String
 )
