a5bdbe9bc43eb069a160b5a192ec8004d455b7ad
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-06-16 02:24:00 -0300

    Adicionado comentários explicando a funcionalidade das funções

--- app/src/main/java/dev/jvitor/gerenciadordematriculas/view/viewholder/MainViewHolder.kt
+++ app/src/main/java/dev/jvitor/gerenciadordematriculas/view/viewholder/MainViewHolder.kt
@@ -8,6 +8,7 @@ import dev.jvitor.gerenciadordematriculas.view.listener.OnAlunoListener
 
 class MainViewHolder(private val bind: CardModelBinding, private val listener: OnAlunoListener ) : RecyclerView.ViewHolder(bind.root) {
 
+    // Altera as informações a serem adicionadas para
     @SuppressLint("SetTextI18n")
     fun bind(aluno: Aluno) {
         bind.textDoName.text = " ${aluno.name}"
@@ -21,7 +22,5 @@ class MainViewHolder(private val bind: CardModelBinding, private val listener: O
         bind.iconDelete.setOnClickListener {
             listener.onDelete(aluno.cpf)
         }
-
     }
-
 }
