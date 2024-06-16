ae1108a8b3860ec2bab4c31bb58e66a65e46bfc3
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-06-16 02:30:57 -0300

    Adicionado comentários explicando a funcionalidade das funções

--- app/src/main/java/dev/jvitor/gerenciadordematriculas/model/Constants.kt
+++ app/src/main/java/dev/jvitor/gerenciadordematriculas/model/Constants.kt
@@ -7,14 +7,11 @@ class Constants private constructor() {
         const val NAME = "name"
         const val SPORT = "sport"
         const val DAY = "day"
-
     }
 
     object Database{
         const val DATABASE_NAME = "aluno-db"
         const val QUERY_ALL = "SELECT * FROM Aluno ORDER BY name ASC"
         const val QUERY_GET = "SELECT * FROM Aluno WHERE cpf = :cpf"
-
     }
-
 }
