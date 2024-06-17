fc47acc401bd4b48048a79cd42fb0f8867513fe9
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-06-16 23:50:06 -0300

    Adicionado comentários explicando a funcionalidade das funções

--- app/src/main/java/dev/jvitor/gerenciadordematriculas/repository/AlunoRepository.kt
+++ app/src/main/java/dev/jvitor/gerenciadordematriculas/repository/AlunoRepository.kt
@@ -5,12 +5,15 @@ import dev.jvitor.gerenciadordematriculas.model.Aluno
 
 class AlunoRepository(context: Context) {
 
+    // Instancia do Banco de Dados
     private val database = AlunoDatabase.getDatabase(context).alunoDao()
 
+    // Responsável por pegar um aluno específico pelo cpf
     fun get(cpf: String) : Aluno{
         return database.get(cpf)
     }
 
+    // Inserir ou alterar alunos
     suspend fun save(aluno: Aluno){
         if (get(aluno.cpf) == null)
             database.insert(aluno)
@@ -18,10 +21,12 @@ class AlunoRepository(context: Context) {
             database.update(aluno)
     }
 
+    // Pegar todos os alunos cadastrados
     fun getAll() : List<Aluno>{
         return database.getAll()
     }
 
+    // Deletar um aluno específico pelo cpf
    fun delete(cpf: String){
         val aluno = database.get(cpf)
         database.delete(aluno)
