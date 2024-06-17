35511c4ca006bb436e46ddca6d20390114735bce
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-06-16 23:50:14 -0300

    Adicionado comentários explicando a funcionalidade das funções

--- app/src/main/java/dev/jvitor/gerenciadordematriculas/view/viewmodel/MainViewModel.kt
+++ app/src/main/java/dev/jvitor/gerenciadordematriculas/view/viewmodel/MainViewModel.kt
@@ -9,19 +9,24 @@ import kotlinx.coroutines.flow.asStateFlow
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
+    // Instancia do repositorio
     private val repository = AlunoRepository(application.applicationContext)
 
+    // Observadores para lista de alunos
     private val _alunos = MutableStateFlow<List<Aluno>>(emptyList())
     val alunos = _alunos.asStateFlow()
 
+    // Pegar a lista com todos os alunos do banco de dados
     fun getAll() {
         _alunos.value = repository.getAll()
     }
 
+    // Deletar alunos do banco de dados
     fun delete(cpf: String) {
         repository.delete(cpf)
     }
 
+    // Pegar um aluno com cpf
     fun get(cpf: String): Aluno {
         return repository.get(cpf)
     }
